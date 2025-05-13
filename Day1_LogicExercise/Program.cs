28032df5573c931577679169188ddc4c427fe285
minionzzz <fernandoalvin3@gmail.com>
2025-05-13 11:40:54 +0700

    Logic Exercise

---
+++ Day1_LogicExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿        Console.Write("Enter a number: ");
+        if (int.TryParse(Console.ReadLine(), out int n))
+        {
+            for (int x = 1; x <= n; x++)
+            {
+                if (x % 3 == 0 && x % 5 == 0)
+                {
+                    Console.Write("foobar ");
+                }
+                else if (x % 3 == 0)
+                {
+                    Console.Write("foo ");
+                }
+                else if (x % 5 == 0)
+                {
+                    Console.Write("bar ");
+                }
+                else
+                {
+                    Console.Write(x + ", ");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
