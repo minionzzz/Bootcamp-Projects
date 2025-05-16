c708d82ee4bfde8a42867692b03cca613f85a476
minionzzz <fernandoalvin3@gmail.com>
2025-05-16 15:01:20 +0700

    Day4Enums

---
+++ Day4_EnumFlags/Program.cs
@@ -0,0 +1,25 @@
+﻿[Flags]
+enum FileAccess
+{
+    None    = 0,
+    Read    = 1,  // 0001
+    Write   = 2,  // 0010
+    Execute = 4,  // 0100
+    All     = Read | Write | Execute
+}
+
+class Program{
+    static void Main(){
+        var access = FileAccess.Read | FileAccess.Write;
+        Console.WriteLine(access);  // Output: Read, Write
+
+        // Cek apakah akses memiliki Read
+        if ((access & FileAccess.Read) == FileAccess.Read)
+        {
+            Console.WriteLine("Read access granted.");
+        }
+        else{
+            Console.WriteLine("Invalid");
+        }
+    }
+}
---
+++ Day4_Enums/Program.cs
@@ -0,0 +1,39 @@
+﻿enum Day
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+class Program{
+    static void Main(){
+        Day today = Day.Sunday;
+
+        if (today == Day.Monday){
+            Console.WriteLine("It's Monday!");
+        }
+        else if(today == Day.Tuesday){
+            Console.WriteLine("It's Tuesday");
+        }
+        else if(today == Day.Wednesday){
+            Console.WriteLine("It's Wednesday");
+        }
+        else if(today == Day.Thursday){
+            Console.WriteLine("It's Thursday");
+        }
+        else if(today == Day.Friday){
+            Console.WriteLine("It's Friday");
+        }
+        else if(today == Day.Saturday){
+            Console.WriteLine("It's Saturday");
+        }
+        else{
+            Console.WriteLine("It's Sunday");
+        }
+    }
+}
+
